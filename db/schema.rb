922d54687c38f2a341fe6ecb99fec515a90f31bf
Adan Villarreal <adanvillarreal0@gmail.com>
2018-11-05 09:34:12 -0600

    Adds materialize. Members form complete. GEMFILE WITH POSTGRES.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_04_235213) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string "firstName"
     t.string "lastName"
