ec356db765b3331345dd7d549cd41b98242bc743
Adan Villarreal <adanvillarreal0@gmail.com>
2018-11-11 22:16:56 -0600

    Adds seeds and changes redirects for payments to show.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names = ['Juan', 'Pedro', 'Jesus', 'Martin', 'Luis', 'Maria', 'Paco', 'Adan', 'Maria', 'Josefa', 'Ana', 'Sofia', 'Javier', 'Antonio', 'Jose', 'Matias', 'Alberto', 'Eva']
+surenames = ['Chavez', 'Gonzalez', 'Martinez', 'Alvarez', 'Garza']
+x = 1
+names.each do |name|
+  Member.create(
+    firstName: name,
+    lastName: surenames.sample,
+    email: "#{name.downcase}@mail.com",
+    phoneNumber: "123456789",
+    active: true,
+    paymentDay: 3,
+    nextPaymentDate: Time.now() + x.days)
+    x = x + 1
+end
